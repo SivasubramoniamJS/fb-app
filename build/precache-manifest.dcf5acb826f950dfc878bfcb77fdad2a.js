779f3e79cf481e83587283b0a25f20ab0843a05f
SivasubramoniamJS <73070410+SivasubramoniamJS@users.noreply.github.com>
2021-01-24 12:19:15 +0530

    Add files via upload

---
+++ build/precache-manifest.dcf5acb826f950dfc878bfcb77fdad2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82cae99fb2bd5acf0a21f10a3586c66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "565b8be7bf6ac7d266e1",
+    "url": "/static/css/main.a2603209.chunk.css"
+  },
+  {
+    "revision": "6eaace91e5afe6baf07b",
+    "url": "/static/js/2.07307265.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.07307265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "565b8be7bf6ac7d266e1",
+    "url": "/static/js/main.5557029e.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
